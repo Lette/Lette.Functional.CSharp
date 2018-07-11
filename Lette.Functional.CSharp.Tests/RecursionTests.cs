66bdcb8dc77cd4e5f4fef81db8bb31111d1c4a5d
Christoffer Lette <christoffer.lette@gmail.com>
2018-07-11 14:34:06 +0200

    Created helper classes for tail recursion.

---
+++ Lette.Functional.CSharp.Tests/RecursionTests.cs
@@ -0,0 +1,80 @@
+﻿using System.Numerics;
+using Xunit;
+
+namespace Lette.Functional.CSharp.Tests
+{
+    public class RecursionTests
+    {
+        [Fact]
+        public void Returns_final_result()
+        {
+            RecursionResult<int> F() => RecursionResult<int>.Final(1);
+
+            Assert.Equal(1, TailRecursion.Run(F));
+        }
+
+        [Fact]
+        public void Returns_final_result_from_next_iteration()
+        {
+            RecursionResult<int> F() => RecursionResult<int>.Next(() => RecursionResult<int>.Final(2));
+
+            Assert.Equal(2, TailRecursion.Run(F));
+        }
+
+        [Fact]
+        public void Returns_final_result_after_recursion()
+        {
+            RecursionResult<int> Factorial(int k)
+            {
+                RecursionResult<int> FactorialAcc(int m, int acc)
+                {
+                    if (m == 1)
+                    {
+                        return RecursionResult<int>.Final(acc);
+                    }
+
+                    return RecursionResult<int>.Next(() => FactorialAcc(m - 1, m * acc));
+                }
+
+                return FactorialAcc(k, 1);
+            }
+
+            Assert.Equal(5 * 4 * 3 * 2 * 1, TailRecursion.Run(() => Factorial(5)));
+        }
+
+        [Fact]
+        public void Deep_recursion_does_not_blow_the_stack()
+        {
+            RecursionResult<BigInteger> Factorial(int k)
+            {
+                RecursionResult<BigInteger> FactorialAcc(int m, BigInteger acc)
+                {
+                    if (m == 1)
+                        return RecursionResult<BigInteger>.Final(acc);
+
+                    return RecursionResult<BigInteger>.Next(() => FactorialAcc(m - 1, m * acc));
+                }
+
+                return FactorialAcc(k, 1);
+            }
+
+            var ex = Record.Exception(() => TailRecursion.Run(() => Factorial(20000)));
+
+            Assert.Null(ex);
+        }
+
+        [Fact(Skip = "Use for finding machine specific limit of recursion.")]
+        public void Recursion_depth_test()
+        {
+            BigInteger F(int k, BigInteger acc)
+            {
+                if (k == 1)
+                    return acc;
+
+                return F(k - 1, k * acc);
+            }
+
+            F(18000, 1);
+        }
+    }
+}
---
+++ Lette.Functional.CSharp/Recursion.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Lette.Functional.CSharp
+{
+    public abstract class RecursionResult<T>
+    {
+        public static RecursionResult<T> Final(T result) => new FinalImpl(result);
+        public static RecursionResult<T> Next(Func<RecursionResult<T>> next) => new NextImpl(next);
+
+        public abstract TOut Match<TOut>(Func<T, TOut> final, Func<Func<RecursionResult<T>>, TOut> next);
+
+        private class FinalImpl : RecursionResult<T>
+        {
+            private readonly T _result;
+
+            public FinalImpl(T result)
+            {
+                _result = result;
+            }
+
+            public override TOut Match<TOut>(Func<T, TOut> final, Func<Func<RecursionResult<T>>, TOut> next)
+            {
+                return final(_result);
+            }
+        }
+
+        public class NextImpl : RecursionResult<T>
+        {
+            private readonly Func<RecursionResult<T>> _next;
+
+            public NextImpl(Func<RecursionResult<T>> next)
+            {
+                _next = next;
+            }
+
+            public override TOut Match<TOut>(Func<T, TOut> final, Func<Func<RecursionResult<T>>, TOut> next)
+            {
+                return next(_next);
+            }
+        }
+    }
+
+    public static class TailRecursion
+    {
+        public static T Run<T>(Func<RecursionResult<T>> f)
+        {
+            while (true)
+            {
+                var result = f();
+
+                (bool isFinal, T finalValue, Func<RecursionResult<T>> next) =
+                    result.Match<(bool, T, Func<RecursionResult<T>>)>(
+                        final: x => (true, x, null),
+                        next:  g => (false, default, g));
+
+                if (isFinal)
+                {
+                    return finalValue;
+                }
+
+                f = next;
+            }
+        }
+    }
+}
